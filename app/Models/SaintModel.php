e82ce737e9dd0c2a26ed0c2cb824f987dd15ad7b
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-02-02 14:30:45 +0100

    Created Migration, Factory, Seeder, Model

---
+++ app/Models/SaintModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SaintModel extends Model
+{
+    use HasFactory;
+}
