c2d5d84599d62b2e53abe9b6828389e1ddb9a86e
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-02-02 15:41:09 +0100

    Debug page displayed

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    public function home(){
+
+        return view('pages.home');
+    }
 }
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main-layout')
+
+@section('content')
+    <h1>SANTI</h1>
+@endsection
