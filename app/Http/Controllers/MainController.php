863483eb7a28d95e8b10967cd55d7212edd93e8d
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-02-02 15:55:03 +0100

    Bonus 1 - Displaying data on homepage

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,10 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Saint;
+
 class MainController extends Controller
 {
     public function home(){
 
-        return view('pages.home');
+        $saints = Saint::orderBy('name') -> get();
+
+        $data = [
+
+            'saints' => $saints
+        ];
+
+        return view('pages.home', $data);
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1>SANTI</h1>
+    <h1> USaints: </h1>
+    <div>
+
+        <ul>
+            @foreach ($saints as $saint)
+                
+                <li>
+                    <h4>
+    
+                        St. {{$saint -> name}} 
+                    </h4>
+                    <h6>
+    
+                        known Miracles: {{$saint -> miracles}}
+                    </h6>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
